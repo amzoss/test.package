8a291fe44b23a2bb6f8af74d3467a31662b7420e
Angela Zoss <az49@duke.edu>
2022-02-07 11:33:09 -0500

    adding example

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -3,7 +3,9 @@
 #' @param animal A character string containing the name of the animal
 #' @param sound A character string containing the sound the animal makes
 #'
-#' @return A sentence matching animals with their sounds
+#' @return A sentences matching animals with their sounds
+#'
+#' @examples sound <- animal_sounds("lion","roar")
 animal_sounds <- function(animal, sound) {
   assertthat::assert_that(
     assertthat::is.string(animal),
