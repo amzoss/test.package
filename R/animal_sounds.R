319f584ba41e05ecd7d30cfb4486c40e82cce12a
Angela Zoss <az49@duke.edu>
2022-02-07 11:27:44 -0500

    updates to description and documentation

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,9 @@
+#' Animal sounds sentences
+#'
+#' @param animal A character string containing the name of the animal
+#' @param sound A character string containing the sound the animal makes
+#'
+#' @return A sentence matching animals with their sounds
 animal_sounds <- function(animal, sound) {
   assertthat::assert_that(
     assertthat::is.string(animal),
